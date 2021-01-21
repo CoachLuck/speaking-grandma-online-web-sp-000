b867908f78726b58959f621555f9ec926fa5a52d
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-21 23:12:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(str)
-  upper = str.upcase
+  upper = str.upcase()
   if (upper = str) {
     return (str == "I LOVE YOU GRANDMA!") ? "I LOVE YOU TOO PUMPKIN!" : "NO, NOT SINCE 1938!";
   }
