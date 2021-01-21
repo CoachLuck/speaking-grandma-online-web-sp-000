b0823ee0b05993ce87bd0b18a3b6aca6ebeeb2f2
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-21 23:17:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,10 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(str)
-  upper = str.upcase()
   if str == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
   if str == str.upcase()
     return "NO, NOT SINCE 1938!"
   end
-
   return "HUH?! SPEAK UP, SONNY!"
 end
